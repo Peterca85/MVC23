dbfc5da118f423e2e0f43c710de9ac058fd8ff39
petercar85 <117587523+petercar85@users.noreply.github.com>
2023-04-16 19:31:57 +0200

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC23.Controllers
+{
+    public class ProductsController : Controller
+
+    {
+        public IActionResult Index()
+        //ok
+
+        {
+            return View();
+        }
+
+
+    }
+}
+
